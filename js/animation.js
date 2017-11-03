49745a571c8ac9dcd88453597542a2ff15d1ae7f
Jakub Fedyk <jakubfedyk@gmail.com>
2017-11-03 12:51:13 +0100

    new layout ver 0.8

--- js/animation.js
+++ js/animation.js
@@ -1,3 +1,10 @@
-$('.img-rounded').on('mouseover', function(){
-    $(this).fadeOut().delay(400).fadeIn();
+$(document).on('mouseover', function(){
+    $('.img-rounded').effect('clip', {times: 2}, 'slow');
 });
+
+
+$('.main__text').css('background-image','url(./images/pexels-photo-355770.jpeg');
+
+
+
+$('.header_title, .main__text, .text_cv').css('margin-left', '200px');
