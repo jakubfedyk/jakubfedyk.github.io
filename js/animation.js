5df76c5ca3bceaf61c0590f03d7e1ab56876000c
Jakub Fedyk <jakubfedyk@gmail.com>
2018-01-28 17:24:14 +0100

    from the beggining-grid

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,27 @@
-$(document).on('mouseover', function(){
-    $('.img-rounded').effect('clip', {times: 2}, 'slow');
-});
+$(document)
+    .on('mouseenter', function () {
+        $('.img-rounded').effect('bounce', {times: 2}, 'slow')
+    })
+    .on('mouseleave', function () {
+        $('.img-rounded').show();
+    });
+
+// $(document.body).
 
 
-$('.main__text').css('background-image','url(./images/pexels-photo-355770.jpeg');
+
+$('.header_title').css({
+    'background-image': 'url(./images/pexels-photo-316466.jpeg)'
+}).addClass('img_attr');
+$('.contact').css({
+    'background-image': 'url(./images/pexels-photo-209634.jpeg'
+}).addClass('img_attr');
+$('.right__text').css({
+    'background-image': 'url(./images/pexels-photo-247671.jpeg'
+}).addClass('img_attr');
+$('.main__text').css({
+    'background-image': 'url(./images/pexels-photo-139321.jpeg'
+}).addClass('img_attr');
 
 
-
-$('.header_title, .main__text, .text_cv').css('margin-left', '200px');
+// $('.header_title, .main__text, .text_cv').css('margin-left', '200px');
